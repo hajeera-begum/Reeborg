da3758332d19f34afd668058b3c0cc2750332f51
Hajeera <hajeerajobs@gmail.com>
2025-12-16 20:21:38 +0530

    Refactor movement functions and add wall navigation

--- Home4.py
+++ Home4.py
@@ -2,21 +2,26 @@ def jump():
     move()
     move()
     move()
+​
+def inner_wall():
+    turn_left()
+    jump()
+​
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
-def forward():
-    jump()
-    turn_left()
-    jump()
+​
+def outer_wall():
     turn_right()
     move()
     turn_right()
 ​
 for i in range(3):
-    forward()
-                   
+    jump()
+    inner_wall()
+    outer_wall()
+​
 jump()
-turn_left()
-jump()
+inner_wall()
+    
