17695d29d3eb45e7e96cc6f4da485d49fb293d35
Hajeera <hajeerajobs@gmail.com>
2025-12-17 22:03:39 +0530

    Add navigation logic in Around4.py
    Implement a navigation algorithm with wall detection and object handling.

---
+++ Around4.py
@@ -0,0 +1,20 @@
+#This works for Around1, Around2, Around3
+put()
+​
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+​
+while True:
+    if wall_in_front():
+        turn_left()
+    if right_is_clear():
+        turn_right()
+    if front_is_clear():
+        move()
+    elif is_facing_north():
+        turn_left()
+        move()
+    if object_here():
+        take()
