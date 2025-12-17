af3fc75454074b792e03713aaa74fc46722d1f1d
Hajeera <hajeerajobs@gmail.com>
2025-12-17 12:45:01 +0530

    Refactor movement and object handling logic

--- Around1_apple.py
+++ Around1_apple.py
@@ -1,10 +1,8 @@
 move()
 while not at_goal():
-    if front_is_clear():
-        if object_here():
-            take()
+    if object_here():
+        take()
+    
     if wall_in_front():
         turn_left()
-        if object_here():
-            take()
     move()
