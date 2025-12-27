951da9d33f4d87d80572d7f3804c5efab5bc0bf7
Hajeera <hajeerajobs@gmail.com>
2025-12-27 14:19:45 +0530

    Refactor movement functions and add climb_down
    Refactored movement functions for clarity and added climb_down functionality.

--- Newspaper0.py
+++ Newspaper0.py
@@ -1,34 +1,26 @@
 def turn_right():
     for i in range(3):
         turn_left()
-def move_3steps():
+def climb_up():
     for i in range(2):
         move()
    
 take()
-for i in range(3):
-    turn_left()
-    move()
-    turn_right()
-    move_3steps()
+
+def movement(steps):
+    for i in range(steps):
+        turn_left()
+        move()
+        turn_right()
+        climb_up()
+movement(3)
 put()
+turn_left()
+turn_left()
+def climb_down():
+    climb_up()
+    movement(2)
 
-turn_left()
-turn_left()
-move()
-move()
+climb_down()
 turn_left()
 move()
-turn_right()
-move()
-move()
-turn_left()
-move()
-turn_right()
-move()
-move()
-turn_left()
-move()
-
-
-
