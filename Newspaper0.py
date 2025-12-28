a49c762553811347e91b3166a777b4ac78dab65c
Hajeera <hajeerajobs@gmail.com>
2025-12-28 12:14:52 +0530

    Refactor climbing and movement functions

--- Newspaper0.py
+++ Newspaper0.py
@@ -1,26 +1,21 @@
 def turn_right():
     for i in range(3):
         turn_left()
-def climb_up():
+def up_down():
+    turn_left()
+    move()
+    turn_right()
+def movement():
     for i in range(2):
         move()
-   
 take()
-
-def movement(steps):
-    for i in range(steps):
-        turn_left()
-        move()
-        turn_right()
-        climb_up()
-movement(3)
+for i in range(3):
+    up_down()
+    movement()
 put()
 turn_left()
 turn_left()
-def climb_down():
-    climb_up()
-    movement(2)
-
-climb_down()
-turn_left()
-move()
+​
+for i in range(3):
+    movement()
+    up_down()
