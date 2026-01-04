b5ac6cca05e79349dc7cd3412dc363027945e731
Hajeera <hajeerajobs@gmail.com>
2026-01-04 18:50:14 +0530

    Add Storm2.py with robot movement and collection logic

---
+++ Storm2.py
@@ -0,0 +1,42 @@
+
+def turn_right():
+    repeat 3:
+        turn_left()
+def collect():
+    while object_here():
+        take()
+    move()
+def roam(steps):
+    for i in range(steps):
+        collect()
+move()
+roam(4)
+turn_left()
+roam(5)
+repeat 2:
+    turn_left()
+    collect()
+    turn_left()
+    collect()
+    roam(3)
+    turn_right()
+    move()
+    turn_right()
+    collect()
+    roam(3)
+turn_left()
+collect()
+turn_left()
+roam(3)
+while carries_object():
+    toss()
+    
+turn_left()
+collect()
+turn_right()
+move()
+move()
+turn_right()
+move()
+​
+    
